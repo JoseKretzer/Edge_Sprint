2c8067977e64e51e678d6305f101e262fa806f06
JoseKretzer <162123548+JoseKretzer@users.noreply.github.com>
2024-09-30 18:42:12 -0300

    Codigo.ino

--- Codigo.ino
+++ Codigo.ino
@@ -1,117 +1,120 @@
 #include <WiFi.h>
-#include <WebServer.h>
-
-// Defina suas credenciais de Wi-Fi
-const char* ssid = "SEU_SSID";       
-const char* password = "SUA_SENHA";
-
-// Pinos onde os sensores de RPM e Velocímetro estão conectados
+#include <PubSubClient.h>
+ 
+// Configurações - variáveis editáveis
+const char* default_SSID = "Wokwi-GUEST"; // Nome da rede Wi-Fi
+const char* default_PASSWORD = ""; // Senha da rede Wi-Fi
+const char* default_BROKER_MQTT = "46.17.108.113"; // IP do Broker MQTT
+const int default_BROKER_PORT = 1883; // Porta do Broker MQTT
+const char* default_TOPICO_PUBLISH_RPM = "/TEF/xxx001/attrs/rpm"; // Tópico MQTT para RPM
+const char* default_TOPICO_PUBLISH_VELOCIDADE = "/TEF/xxx001/attrs/velocidade"; // Tópico MQTT para Velocidade
+const char* default_ID_MQTT = "fiware_001"; // ID MQTT
+ 
+// Pinos dos sensores
 const int hallPinRPM = 18;  
 const int hallPinVelocimetro = 5;  
-
-// Variáveis para o conta-giros (RPM)
-int lastStateRPM = HIGH;
-unsigned long lastPulseTimeRPM = 0;
-unsigned long currentTimeRPM = 0;
-float rpm = 0;
-unsigned long timeoutRPM = 2000;  
-
+ 
+// Variáveis para o conta-giros
+int lastStateRPM = HIGH;  
+unsigned long lastPulseTimeRPM = 0;  
+float rpm = 0;  
+ 
 // Variáveis para o velocímetro
-int lastStateVelocimetro = HIGH;
-unsigned long lastPulseTimeVelocimetro = 0;
-unsigned long currentTimeVelocimetro = 0;
-float velocidade = 0;
-unsigned long timeoutVelocimetro = 2000;  
-
-// Parâmetros do velocímetro
-float diameterVelocimetro = 0.5;  // Diâmetro da roda (ajuste conforme necessário)
+int lastStateVelocimetro = HIGH;  
+unsigned long lastPulseTimeVelocimetro = 0;  
+float velocidade = 0;  
+float diameterVelocimetro = 0.5; // Diâmetro da roda em metros
 const float pi = 3.1416;
-
-// Cria um servidor web na porta 80
-WebServer server(80);
-
-// Função para calcular o RPM
-void calculaRPM() {
-  currentTimeRPM = millis();
-
-  int currentStateRPM = digitalRead(hallPinRPM);
-
-  if (lastStateRPM == LOW && currentStateRPM == HIGH) {
-    currentTimeRPM = millis();
-
-    if (lastPulseTimeRPM > 0) {
-      unsigned long timeDifferenceRPM = currentTimeRPM - lastPulseTimeRPM;
-      rpm = (60000.0 / timeDifferenceRPM);
-
-      Serial.print("RPM: ");
-      Serial.println(rpm);
-    }
-    lastPulseTimeRPM = currentTimeRPM;
-  }
-  lastStateRPM = currentStateRPM;
-}
-
-// Função para calcular a velocidade
-void calculaVelocidade() {
-  currentTimeVelocimetro = millis();
-
-  int currentStateVelocimetro = digitalRead(hallPinVelocimetro);
-
-  if (lastStateVelocimetro == LOW && currentStateVelocimetro == HIGH) {
-    currentTimeVelocimetro = millis();
-
-    if (lastPulseTimeVelocimetro > 0) {
-      unsigned long timeDifferenceVelocimetro = currentTimeVelocimetro - lastPulseTimeVelocimetro;
-
-      float rpmVelocimetro = (60000.0 / timeDifferenceVelocimetro);
-      float circumferenceVelocimetro = pi * diameterVelocimetro;
-      float linearVelocity = (circumferenceVelocimetro * rpmVelocimetro) / 60.0;
-      velocidade = linearVelocity * 3.6;
-
-      Serial.print("Velocidade: ");
-      Serial.print(velocidade);
-      Serial.println(" km/h");
-    }
-    lastPulseTimeVelocimetro = currentTimeVelocimetro;
-  }
-  lastStateVelocimetro = currentStateVelocimetro;
-}
-
-// Função para configurar a rede Wi-Fi e servidor web
-void setupWiFi() {
-  Serial.begin(115200);
-  
-  // Conectar ao Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Conectando ao Wi-Fi...");
-  }
-
-  Serial.println("Conectado ao Wi-Fi");
-  Serial.println(WiFi.localIP());  // Exibe o IP do ESP32 na rede
-
-  // Configurar rota HTTP para enviar dados de RPM e velocidade
-  server.on("/dados", [](){
-    String jsonData = "{\"rpm\": " + String(rpm) + ", \"velocidade\": " + String(velocidade) + "}";
-    server.send(200, "application/json", jsonData);
-  });
-
-  // Iniciar o servidor
-  server.begin();
-}
-
+ 
+// Inicializa cliente WiFi e MQTT
+WiFiClient espClient;
+PubSubClient MQTT(espClient);
+ 
 void setup() {
-  Serial.begin(9600);  // Inicializa a comunicação serial
-  pinMode(hallPinRPM, INPUT_PULLUP);
-  pinMode(hallPinVelocimetro, INPUT_PULLUP);
-
-  setupWiFi();  // Configura o Wi-Fi e inicia o servidor
+    Serial.begin(115200);  
+    pinMode(hallPinRPM, INPUT_PULLUP);  
+    pinMode(hallPinVelocimetro, INPUT_PULLUP);  
+ 
+    // Conecta ao Wi-Fi
+    WiFi.begin(default_SSID, default_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Conectando ao Wi-Fi...");
+    }
+    Serial.println("Conectado ao Wi-Fi!");
+ 
+    // Configura o MQTT
+    MQTT.setServer(default_BROKER_MQTT, default_BROKER_PORT);
 }
-
+ 
 void loop() {
-  calculaRPM();
-  calculaVelocidade();
-  server.handleClient();  // Lida com as requisições HTTP
-  delay(10);  // Pequeno atraso para suavizar as leituras
+    verificaConexoesWiFiEMQTT();
+    calculaRPM();
+    calculaVelocidade();
+    enviaDadosMQTT();
+    MQTT.loop();
+    delay(100);
+}
+ 
+void verificaConexoesWiFiEMQTT() {
+    if (!MQTT.connected()) {
+        while (!MQTT.connected()) {
+            Serial.print("Tentando se conectar ao broker MQTT...");
+            if (MQTT.connect(default_ID_MQTT)) {
+                Serial.println("Conectado ao broker!");
+            } else {
+                Serial.print("Falha, rc=");
+                Serial.print(MQTT.state());
+                Serial.println(" Tentando novamente em 2 segundos...");
+                delay(2000);
+            }
+        }
+    }
+}
+ 
+void calculaRPM() {
+    int currentStateRPM = digitalRead(hallPinRPM);
+    if (lastStateRPM == LOW && currentStateRPM == HIGH) {
+        unsigned long currentTimeRPM = millis();
+        if (lastPulseTimeRPM > 0) {
+            unsigned long timeDifferenceRPM = currentTimeRPM - lastPulseTimeRPM;
+            rpm = (60000.0 / timeDifferenceRPM);
+            Serial.print("RPM: ");
+            Serial.println(rpm);
+        }
+        lastPulseTimeRPM = currentTimeRPM;
+    }
+    lastStateRPM = currentStateRPM;
+}
+ 
+void calculaVelocidade() {
+    int currentStateVelocimetro = digitalRead(hallPinVelocimetro);
+    if (lastStateVelocimetro == LOW && currentStateVelocimetro == HIGH) {
+        unsigned long currentTimeVelocimetro = millis();
+        if (lastPulseTimeVelocimetro > 0) {
+            unsigned long timeDifferenceVelocimetro = currentTimeVelocimetro - lastPulseTimeVelocimetro;
+            float rpmVelocimetro = (60000.0 / timeDifferenceVelocimetro);
+            float circumferenceVelocimetro = pi * diameterVelocimetro;
+            float linearVelocity = (circumferenceVelocimetro * rpmVelocimetro) / 60.0;
+            velocidade = linearVelocity * 3.6; // Converte de m/s para km/h
+            Serial.print("Velocidade: ");
+            Serial.print(velocidade);
+            Serial.println(" km/h");
+        }
+        lastPulseTimeVelocimetro = currentTimeVelocimetro;
+    }
+    lastStateVelocimetro = currentStateVelocimetro;
+}
+ 
+void enviaDadosMQTT() {
+    String mensagemRPM = String(rpm);
+    String mensagemVelocidade = String(velocidade);
+ 
+    MQTT.publish(default_TOPICO_PUBLISH_RPM, mensagemRPM.c_str());
+    MQTT.publish(default_TOPICO_PUBLISH_VELOCIDADE, mensagemVelocidade.c_str());
+ 
+    Serial.print("Dados enviados - RPM: ");
+    Serial.print(mensagemRPM);
+    Serial.print(", Velocidade: ");
+    Serial.println(mensagemVelocidade);
 }
